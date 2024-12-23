f783d3bae9a9a24fbcf0936bb1963982992a648e
Hari Narasimhan <narasimhan3737@gmail.com>
2024-12-23 23:16:42 +0000

    icebreaker

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,5 +1,7 @@
 import os
 from typing import Tuple
+
+from langchain_openai import ChatOpenAI
 from agents import linkedin_lookup_agent
 from dotenv import load_dotenv
 from langchain.prompts.prompt import PromptTemplate
@@ -37,9 +39,9 @@ def ice_break_with(name: str) -> Tuple:
         },
     )
 
-    # llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-    # llm = ChatOllama(model="llama3")
-    llm = ChatOllama(model="mistral")
+    #llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    llm = ChatOllama(model="llama3")
+    #llm = ChatOllama(model="mistral")
 
     # chain = summary_prompt_template | llm | StrOutputParser()
     chain = summary_prompt_template | llm | summary_parser
