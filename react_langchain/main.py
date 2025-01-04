fe94015a7dbd17da2f34c3fd2a9647d35dc227c7
Hari Narasimhan <narasimhan3737@gmail.com>
2025-01-04 19:52:35 +0000

    Reasoning Agent

--- react_langchain/main.py
+++ react_langchain/main.py
@@ -1,10 +1,58 @@
 from dotenv import load_dotenv
+from langchain.agents import tool
+from langchain.prompts import PromptTemplate
+from langchain.tools.render import render_text_description
+from langchain.agents.output_parsers import ReActSingleInputOutputParser
+from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
 load_dotenv()
 
+@tool
 def get_text_length(text:str)-> int:
     """Returns the length of a text by characters"""
+    print(f"get_text_length enter with {text=}")
+    text = text.strip("'\n").strip(
+        '"'
+    ) #stripping non alphabetic characters just in case
     return len(text)
 
 if __name__ == "__main__":
     print("Hello ReAct LangChain!")
-    print(get_text_length(text="Dog"))
+    #print(get_text_length(text="Dog"))
+
+    tools =[get_text_length]
+
+    template = """
+    Answer the following questions as best you can. You have access to the following tools:
+
+    {tools}
+
+    Use the following format:
+
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+
+    Begin!
+
+    Question: {input}
+    Thought:
+    """
+
+    prompt= PromptTemplate.from_template(template=template).partial(
+        tools=render_text_description(tools), tool_names=", ".join([t.name for t in tools])
+    )
+
+    #llm = ChatOpenAI(temperature=0, stop=["\nObservation"])
+    llm = ChatOllama(model="llama3", temperature=0, stop=["\nObservation"])
+
+    agent = {"input": lambda x:x["input"]} | prompt | llm | ReActSingleInputOutputParser()
+
+    res = agent.invoke({"input": "What is the length of 'DOG' in characters?'"})
+    print(res)
+
