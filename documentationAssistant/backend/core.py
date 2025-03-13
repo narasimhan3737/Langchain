7408dad24457c87d766ac3351cc6e07b77a4a414
Hari Narasimhan <narasimhan3737@gmail.com>
2025-03-13 14:46:22 +0000

    Added Webpage

--- documentationAssistant/backend/core.py
+++ documentationAssistant/backend/core.py
@@ -1,4 +1,6 @@
+from ast import Dict, List
 import os
+from typing import Any
 from dotenv import load_dotenv
 from langchain.chains.retrieval import create_retrieval_chain
 from openai import chat
@@ -10,23 +12,30 @@ load_dotenv()
 
 from langchain import hub
 from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
 from langchain_pinecone import PineconeVectorStore
+from typing import List,Dict, Any
 
 
-def run_llm(query: str):
+def run_llm(query: str, chat_history: List[Dict[str, Any]]):
     embeddings = HuggingFaceEmbeddings(model_name="intfloat/multilingual-e5-large")
     docsearch = PineconeVectorStore(
         index_name=os.environ["INDEX_NAME"], embedding=embeddings
     )
-    chat = Ollama(model="deepseek-r1:8b")
+    chat = Ollama(model="deepseek-r1:14b")
 
     retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
     stuff_documents_chain = create_stuff_documents_chain(chat, retrieval_qa_chat_prompt)
 
-    qa = create_retrieval_chain(
-        retriever=docsearch.as_retriever(), combine_docs_chain=stuff_documents_chain
+    rephrase_prompt = hub.pull("langchain-ai/chat-langchain-rephrase")
+    history_aware_retriever = create_history_aware_retriever(
+        llm=chat, retriever=docsearch.as_retriever(), prompt=rephrase_prompt
     )
-    result = qa.invoke(input={"input": query})
+
+    qa = create_retrieval_chain(
+        retriever=history_aware_retriever, combine_docs_chain=stuff_documents_chain
+    )
+    result = qa.invoke(input={"input": query, "chat_history": chat_history})
     new_result = {
         "query": result["input"],
         "result": result["answer"],
@@ -37,4 +46,5 @@ def run_llm(query: str):
 
 if __name__ == "__main__":
     res = run_llm(query="What is a LangChain Chain?")
-    print(res["answer"])
+    print(res['result'])
+    #print(res["answer"])
